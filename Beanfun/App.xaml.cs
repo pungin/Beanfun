ff4df519125e678bfbd43edbc8a114ed99f4cd49
lshw54 <wl1999054@gmail.com>
2026-03-31 18:52:50 +0800

    pipeline: cleanup

--- Beanfun/App.xaml.cs
+++ Beanfun/App.xaml.cs
@@ -131,7 +131,9 @@ namespace Beanfun
 
         public static int ReleaseResource(string file)
         {
-            string baseDir = Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+            string baseDir = Path.GetDirectoryName(
+                System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName
+            );
             string path = Path.Combine(baseDir, file);
             using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(file))
             {
--- Beanfun/MainWindow.xaml.cs
+++ Beanfun/MainWindow.xaml.cs
@@ -1857,8 +1857,12 @@ namespace Beanfun
                     try
                     {
                         var proc = new Process();
-                        proc.StartInfo.FileName =
-                            Path.Combine(Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName), "LRProc.exe");
+                        proc.StartInfo.FileName = Path.Combine(
+                            Path.GetDirectoryName(
+                                System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName
+                            ),
+                            "LRProc.exe"
+                        );
                         proc.StartInfo.Arguments =
                             "ef3e7b42-a87c-4c07-ae3e-eeebeef12762 " + commandLine;
                         proc.StartInfo.WorkingDirectory = Path.GetDirectoryName(path);
