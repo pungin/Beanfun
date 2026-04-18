4ecd5bbf19cd40283f35602e2515b3733f447604
lshw54 <wl1999054@gmail.com>
2026-04-18 15:06:07 +0800

    feat: TOTP form paste support, backspace navigation, and digit filter

--- Beanfun/Pages/LoginTotp.xaml.cs
+++ Beanfun/Pages/LoginTotp.xaml.cs
@@ -1,28 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Beanfun
 {
-    /// <summary>
-    /// LoginTotp.xaml 的交互逻辑
-    /// </summary>
     public partial class LoginTotp : Page
     {
+        private TextBox[] _otpBoxes;
+        private bool _isPasting;
+
         public LoginTotp()
         {
             InitializeComponent();
+            _otpBoxes = new[] { otp1, otp2, otp3, otp4, otp5, otp6 };
         }
 
         private void btn_login_Click(object sender, RoutedEventArgs e)
@@ -37,65 +28,94 @@ namespace Beanfun
             App.MainWnd.frame.Content = App.MainWnd.loginPage;
         }
 
-        private void otp1_PreviewKeyUp(object sender, KeyEventArgs e)
+        private void otp_PreviewKeyDown(object sender, KeyEventArgs e)
         {
-            if (otp1.Text.Length > 0)
-            {
-                otp2.Focus();
-            }
-        }
+            var box = sender as TextBox;
+            int index = System.Array.IndexOf(_otpBoxes, box);
 
-        private void otp2_PreviewKeyUp(object sender, KeyEventArgs e)
-        {
-            if (otp2.Text.Length > 0)
+            if (e.Key == Key.Back && box.Text.Length == 0 && index > 0)
             {
-                otp3.Focus();
+                _otpBoxes[index - 1].Text = "";
+                _otpBoxes[index - 1].Focus();
+                e.Handled = true;
+                return;
             }
-        }
 
-        private void otp3_PreviewKeyUp(object sender, KeyEventArgs e)
-        {
-            if (otp3.Text.Length > 0)
-            {
-                otp4.Focus();
-            }
-        }
-
-        private void otp4_PreviewKeyUp(object sender, KeyEventArgs e)
-        {
-            if (otp4.Text.Length > 0)
-            {
-                otp5.Focus();
-            }
-        }
-
-        private void otp5_PreviewKeyUp(object sender, KeyEventArgs e)
-        {
-            if (otp5.Text.Length > 0)
-            {
-                otp6.Focus();
-            }
-        }
-
-        private void otp6_PreviewKeyUp(object sender, KeyEventArgs e)
-        {
             if (
-                otp1.Text.Length > 0
-                && otp2.Text.Length > 0
-                && otp3.Text.Length > 0
-                && otp4.Text.Length > 0
-                && otp5.Text.Length > 0
-                && otp6.Text.Length > 0
+                e.Key == Key.V
+                && (Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control
             )
             {
-                btn_login.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+                e.Handled = true;
+                HandlePaste();
             }
+        }
+
+        private void HandlePaste()
+        {
+            string text = "";
+            try
+            {
+                text = Clipboard.GetText();
+            }
+            catch
+            {
+                return;
+            }
+            string digits = new string(text.Where(char.IsDigit).ToArray());
+            if (digits.Length == 0)
+                return;
+
+            _isPasting = true;
+            for (int i = 0; i < _otpBoxes.Length && i < digits.Length; i++)
+            {
+                _otpBoxes[i].Text = digits[i].ToString();
+            }
+            _isPasting = false;
+
+            int focusIndex = System.Math.Min(digits.Length, _otpBoxes.Length - 1);
+            _otpBoxes[focusIndex].Focus();
+
+            TryAutoSubmit();
+        }
+
+        private void otp_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (_isPasting)
+                return;
+
+            var box = sender as TextBox;
+            int index = System.Array.IndexOf(_otpBoxes, box);
+
+            // Filter non-digit
+            string digits = new string(box.Text.Where(char.IsDigit).ToArray());
+            if (digits != box.Text)
+            {
+                box.Text = digits;
+                box.CaretIndex = digits.Length;
+                return;
+            }
+
+            if (box.Text.Length == 1 && index < _otpBoxes.Length - 1)
+            {
+                _otpBoxes[index + 1].Focus();
+            }
+
+            TryAutoSubmit();
         }
 
         private void otp_GotFocus(object sender, RoutedEventArgs e)
         {
             TextBox box = sender as TextBox;
             box.SelectAll();
+        }
+
+        private void TryAutoSubmit()
+        {
+            if (btn_login.IsEnabled && _otpBoxes.All(b => b.Text.Length == 1))
+            {
+                btn_login.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            }
         }
     }
 }
